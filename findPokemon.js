66c298d4d3aa28d1f4782ab5356e6bda8ca4bf29
Christopher Marsh <christopher.marsh@twinkl.co.uk>
2020-09-18 10:45:13 +0100

    get some pikachu data from pokeApi

--- findPokemon.js
+++ findPokemon.js
@@ -1,6 +1,6 @@
 // var pageContent = document.getElementsByTagName("p");
 var pageContent = document.documentElement.innerHTML;
-
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu').then(response => response.json()).then(json => console.log(json))
 // var words = new Array();
 var pokemon = new Array();
 var pikaCount =  pageContent.search('Pikachu');
